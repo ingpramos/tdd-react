09d4bc198550055d3eb7ebc422cb075a2e9392c8
Pablo Ramos <ingpramos@outlook.com>
2022-04-07 17:50:59 -0500

    Form Interactions

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,22 +1,54 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 class SignUpPage extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Sign Up</h1>
-                <label htmlFor="username">Username</label>
-                <input id="username"/>
-                <label htmlFor="email">E-mail</label>
-                <input id='email'/>
-                <label htmlFor="password">Password</label>
-                <input id='password' type='password'/>
-                <label htmlFor="passwordRepeat">Password Repeat</label>
-                <input id='passwordRepeat' type='password'/>
-                <button disabled>Sign Up</button>
-            </div>
-        )
+  state = {};
+
+  onChangePassword = (event) => {
+    const currentValue = event.target.value;
+    this.setState({
+      password: currentValue,
+    });
+  };
+
+  onChangePasswordRepeat = (event) => {
+    const currentValue = event.target.value;
+    this.setState({
+      passwordRepeat: currentValue,
+    });
+  };
+
+  render() {
+    let disabled = true;
+    const { password, passwordRepeat } = this.state;
+    if (password && passwordRepeat) {
+      disabled = password !== passwordRepeat;
     }
+
+    // if (password === undefined || passwordRepeat === undefined) {
+    //   disabled = true;
+    // } else if (password === passwordRepeat) {
+    //   disabled = false;
+    // } else disabled = true;
+
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <label htmlFor="username">Username</label>
+        <input id="username" />
+        <label htmlFor="email">E-mail</label>
+        <input id="email" />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={this.onChangePassword} />
+        <label htmlFor="passwordRepeat">Password Repeat</label>
+        <input
+          id="passwordRepeat"
+          type="password"
+          onChange={this.onChangePasswordRepeat}
+        />
+        <button disabled={disabled}>Sign Up</button>
+      </div>
+    );
+  }
 }
 
 export default SignUpPage;
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,53 +1,64 @@
-import SignUpPage from './SignUpPage';
-import {render, screen} from '@testing-library/react';
+import SignUpPage from "./SignUpPage";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('Sign Up Page', () => {
-    describe('Layout', () => {
-
-        it('has header', () => {
-            render(<SignUpPage />);
-            const header = screen.queryByRole('heading', {name: 'Sign Up'});
-            expect(header).toBeInTheDocument();
-        });
-        it('has username input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('Username');
-            expect(input).toBeInTheDocument();
-        });
-        it('has email input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('E-mail');
-            expect(input).toBeInTheDocument();
-        });
-        it('has password input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('Password');
-            expect(input).toBeInTheDocument();
-        });
-        it('has password type for password input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('Password');
-            expect(input.type).toBe('password');
-        });
-        it('has password repeat input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('Password Repeat');
-            expect(input).toBeInTheDocument();
-        });
-        it('has password type for password repeat input', () => {
-            render(<SignUpPage />) 
-            const input = screen.getByLabelText('Password Repeat');
-            expect(input.type).toBe('password');
-        });
-        it('has Sign Up button', () => {
-            render(<SignUpPage />);
-            const button = screen.queryByRole('button', {name: 'Sign Up'});
-            expect(button).toBeInTheDocument();
-        });
-        it('disables the button initially', () => {
-            render(<SignUpPage />);
-            const button = screen.queryByRole('button', {name: 'Sign Up'});
-            expect(button).toBeDisabled();
-        });
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has header", () => {
+      render(<SignUpPage />);
+      const header = screen.queryByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
     });
+    it("has username input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Username");
+      expect(input).toBeInTheDocument();
+    });
+    it("has email input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("E-mail");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has password repeat input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password repeat input", () => {
+      render(<SignUpPage />);
+      const input = screen.getByLabelText("Password Repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has Sign Up button", () => {
+      render(<SignUpPage />);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
+    });
+    it("disables the button initially", () => {
+      render(<SignUpPage />);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
+    });
+  });
+  describe("Interactions", () => {
+    it("enable the button when password and password repeat fields have same value", () => {
+      render(<SignUpPage />);
+      const passwordInput = screen.getByLabelText("Password");
+      const passwordRepeatInput = screen.getByLabelText("Password Repeat");
+      userEvent.type(passwordInput, "Qfsd15164");
+      userEvent.type(passwordRepeatInput, "Qfsd15164");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeEnabled();
+    });
+  });
 });
